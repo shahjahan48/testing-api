22c4f44d06a3b1d59665f6e8d920a1f4baa604a2
shahjahan48 <shahjahan.cseku11@gmail.com>
2022-07-24 01:01:56 +0600

    old file removed

--- README.md
+++
@@ -1,2 +0,0 @@
-# testing-api
-This api will be used to test and serve demo data to client application like a proper production grade endpoints
--- src/controllers/api.js
+++
@@ -1,6 +0,0 @@
-const repository = require('../repositories/UsersRepository');
-var user = require('../models/Users');
-
-repository.getUsers().then(results=>{
-    console.log(results);
-})
